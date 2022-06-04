d1cbe09dcb7c44f6133f875fbd8256949ff36216
Mher Hovhannisyan <mh3r99@gmail.com>
2022-06-05 01:31:46 +0400

    meal item form

--- src/components/Meals/AvailableMeals.jsx
+++ src/components/Meals/AvailableMeals.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import classes from "./AvailableMeals.module.css";
-import Card from "../UI/Card";
+import Card from "../UI/Card/Card";
 import MealItem from "./MealItem/MealItem";
 
 const DUMMY_MEALS = [
